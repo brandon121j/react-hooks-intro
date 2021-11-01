c85a24a01ebc167896e7c02273a93b2a2cb0893c
Brandon <brandon121j@gmail.com>
2021-11-01 14:34:08 -0400

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
 
-  const [addOne, setAddOne] = useState(0)
+  const [addOne, setAddOne] = useState(0);
+  const [firstName, setFirstName] = useState("")
 
 
 
@@ -12,6 +13,9 @@ function App() {
       Starting Value: {addOne}
       <button onClick={() => setAddOne(addOne + 1)}>+</button>
       <button onClick={() => setAddOne(addOne - 1)}>-</button>
+      <hr/>
+      <input name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+      <button onClick={() => console.log(firstName)}>Submit</button>
     </div>
   );
 }
